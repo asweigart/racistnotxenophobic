5d192e101769e76138201cc8eafaa86130e3414f
Victor Veytsman <vityavv@gmail.com>
2017-11-22 17:24:32 -0500

    Compact regexes with the same result

--- content.js
+++ content.js
@@ -8,7 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/Xenophobia/g, 'Racism').replace(/Xenophobism/g, 'Racism').replace(/Xenophobic/g, 'Racist').replace(/xenophobia/gi, 'racism').replace(/xenophobism/gi, 'racism').replace(/xenophobic/gi, 'racist');
+            var replacedText = text.replace(/Xenophobia|Xenophobism/g, 'Racism').replace(/Xenophobic/g, 'Racist').replace(/xenophobia|xenophobism/gi, 'racism').replace(/xenophobic/gi, 'racist');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
